abe6748c888f0e929969da2aadf182279b1c00d4
JonathanRys <jonathan.rk.rys@gmail.com>
2023-09-13 12:53:33 -0400

    get loading spinner working

--- src/app/PhysGPT.tsx
+++ src/app/PhysGPT.tsx
@@ -1,17 +1,24 @@
 import './physgpt.css';
 import { useState } from 'react';
 
-import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import { faPaperPlane, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { Overlay, Modal } from '../components/Modal'
 
 const PhysGPT = () => {
     const [modalOpen, setModalOpen] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const searchHandler = () => {
+        setLoading(true);
+        setTimeout(() => alert('I wish it worked too.'), 0);
+        setTimeout(() => setLoading(false), 3000);
+    };
 
     return (
         // This page should validate the user and redirect if unauthenticated.
-        <div className="app-container">
+        <div className={`app-container${loading ? ' loading': ''}`}>
             {
                 modalOpen ? (
                     <Overlay close={() => setModalOpen(false)}>
@@ -62,12 +69,21 @@ const PhysGPT = () => {
                     <section className="gpt-reply">
                         <p>Congratulations, you have exclusive access to the worlds most knowledgable physicist, what would you like to know?</p>
                     </section>
+                    <div className="chat-fader"></div>
                 </div>
             </div>
             <div className="input-container">
                 <label htmlFor="search">Ask her a question</label>
-                <textarea id="search" className="user-input"></textarea>
-                <FontAwesomeIcon className="query-icon" onClick={() => {alert('I wish it worked too.')}} icon={faPaperPlane} title="Send" />
+                <textarea id="search" className={`user-input${loading ? ' disabled' : ''}`}></textarea>
+                <div className={`query-icon${loading ? ' disabled' : ''}`}>
+                    <FontAwesomeIcon
+                        className={loading ? 'disabled' : ''}
+                        onClick={searchHandler}
+                        spin={loading}
+                        icon={loading ? faSpinner : faPaperPlane}
+                        title="Send"
+                    />
+                </div>
             </div>
         </div>
     );
